88a2986c7e7f2b6237b6ebbb78b4c885247e6efd
Christian B <christian@astralclocktowerstudios.com>
2021-12-04 23:11:48 -0500

    Death Barriers, BGM, Enemy

---
+++ Culmination2D_Project/Assets/velocity.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class velocity : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(0f, 2f);
+    }
+}
